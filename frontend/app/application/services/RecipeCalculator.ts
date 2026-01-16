4261fb6050866043bd7d0c12f1bb0263994133aa
ben ali rym <ben.ali.rym@gmail.com>
2026-01-16 17:52:09 +0100

    test

--- frontend/app/application/services/RecipeCalculator.ts
+++ frontend/app/application/services/RecipeCalculator.ts
@@ -1,7 +1,7 @@
 export function calculateIngredients(
     ingredients: any[],
     totalVolume: number
-) {
+): any[] {
     const totalRatio = ingredients.reduce((a, i) => a + i.ratio, 0)
     return ingredients.map((i) => ({
         ...i,
